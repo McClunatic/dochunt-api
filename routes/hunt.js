192082d706f45bc4c38be733b60ecbab00be4b10
Brian McClune <bpmcclune@gmail.com>
2019-10-15 13:45:03 +0000

    changed hunt route to expect HTTP query

--- routes/hunt.js
+++ routes/hunt.js
@@ -5,11 +5,11 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('the-ringer.db', sqlite3.OPEN_READONLY);
 
 /* GET hunt results. */
-router.get("/:target", function(req, res, next) {
+router.get("/", function(req, res, next) {
   db.all("select id, href, title, author, date from articles where "
        + "title like $target or "
        + "subtitle like $target",
-         { $target: '%' + req.params.target + '%' }, (err, rows) => {
+         { $target: '%' + req.query.target + '%' }, (err, rows) => {
            if (err) {
              next(err);
            } else {
