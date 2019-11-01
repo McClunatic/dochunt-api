8ed6d064f8292b773ac30c78855495f4b2137aea
Brian McClune <bpmcclune@gmail.com>
2019-11-01 16:44:44 +0000

    added user route to get user metadata

--- app.js
+++ app.js
@@ -60,10 +60,12 @@ app.use(passport.session());
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
+var userRouter = require('./routes/user');
 var huntRouter = require('./routes/hunt');
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/user', userRouter);
 app.use('/hunt', huntRouter);
 
 // catch 404 and forward to error handler
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const debug = require("debug")("dochunt-api:router:user");
+
+
+/* GET user info */
+router.get("/", passport.authenticate("local"), function(req, res) {
+  return res.send({
+    id: req.user.id,
+    user: req.user.username,
+    email: req.user.email
+  });
+});
+
+module.exports = router;
